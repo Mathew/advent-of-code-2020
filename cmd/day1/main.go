fed0d5c210d6e99982f029746fa462ad7bf1d2b7
Mathew Taylor <mat@mathewtaylor.co.uk>
2020-12-01 23:45:55 +0000

    day 1, part 2.

--- cmd/day1/main.go
+++ cmd/day1/main.go
@@ -7,9 +7,27 @@ import (
 	"log"
 )
 
+func sum(is ...int) int {
+	total := 0
+	for _, i := range is {
+		total += i
+	}
+
+	return total
+}
+
+func multiply(is ...int) int {
+	total := is[0]
+	for _, i := range is[1:] {
+		total = total * i
+	}
+
+	return total
+}
+
 func checkAddition(arr []int, num, answer int) (int, bool) {
 	for _, a := range arr {
-		if a + num == answer {
+		if a+num == answer {
 			return a, true
 		}
 	}
@@ -25,6 +43,27 @@ func findSumCombination(numbers []int, answer int) (int, int, error) {
 	}
 
 	return 0, 0, errors.New("No match found.")
+}
+
+func findComponentSumCombination(numbers []int, answer, numComponents int, path []int) ([]int, bool) {
+	if numComponents == 2 {
+		for i, a := range numbers {
+			if match, ok := checkAddition(numbers[i+1:], sum(path...)+a, answer); ok {
+				path = append(path, a, match)
+				return path, true
+			}
+		}
+	} else {
+		for i, a := range numbers {
+			cp := path
+			cp = append(cp, a)
+			if result, ok := findComponentSumCombination(numbers[i+1:], answer, numComponents-1, cp); ok {
+				return result, true
+			}
+		}
+	}
+
+	return nil, false
 }
 
 func main() {
@@ -46,10 +85,19 @@ func main() {
 		return
 	}
 
+	// Part 1.
 	x, y, err := findSumCombination(ints, 2020)
 	if err != nil {
 		return
 	}
+	log.Printf("Part One:\n%d + %d = %d \n", x, y, x*y)
 
-	log.Printf("%d + %d = %d", x, y, x*y)
+	// Part 2.
+	components, ok := findComponentSumCombination(ints, 2020, 3, nil)
+	log.Print("Part Two: ")
+	if !ok {
+		log.Printf("not found")
+	} else {
+		log.Printf("%v multiplied together = %v", components, multiply(components...))
+	}
 }
